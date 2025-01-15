cdb91bb6115d2351c9be42be30ac968913074221
xmtscf <me@xmts.cf>
2025-01-15 18:46:34 -0300

    update index.js

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ function html(current_drive_order = 0, model = {}) {
     window.current_drive_order = ${current_drive_order};
     window.UI = JSON.parse('${JSON.stringify(uiConfig)}');
   </script>
-  <script src="//cdn.jsdelivr.net/gh/xmtscf/GoIndex@4d1427558a21603e7c7e7dc986a154a9560f4835/app.js"></script>
+  <script src="https://cdn.jsdelivr.net/gh/xmtscf/GoIndex@main/app.js"></script>
 </head>
 <body>
 </body>
